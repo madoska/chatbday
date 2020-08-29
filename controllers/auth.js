3f729fe72868d5f9d2ad9d7d7b0676797df2e752
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-29 16:28:20 +0200

    added function to check if user exists in mongo collection and add them to collection if they dont

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,9 +16,41 @@ passport.use(new facebookStrategy(
         clientID: "618996698804528",
         clientSecret: "3b463b329a47e06b649636ea758107e4",
         callbackURL: "http://localhost:3000/auth/facebook/callback",
-        profileFields: ['id', 'name', 'email', 'picture.type(large)']
-    }, function (accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        return done(null, profile)
+        profileFields: ['id', 'name', 'email', 'picture.type(large)', 'birthday']
+    },
+    (accessToken, refreshToken, profile, done) => {
+        // search for user's ID in mongo collection
+        Users.findOne({ 'id': profile.id }, (err, user) => {
+            // if error, return error
+            if (err) {
+                return done(err);
+            } else { }
+
+            // if user with matching userID found, log them in
+            if (user) {
+                console.log(user)
+                return done(null, user);
+            } else {
+                // if userID does not exist in collection, create new user
+                const newUser = new Users();
+
+                // insert all facebook profile data in our schema
+                newUser.id = profile.id;
+                newUser.firstname = profile.name.givenName;
+                newUser.lastname = profile.name.familyName;
+                newUser.email = profile.emails[0].value;
+                newUser.picture = profile.photos[0].value;
+                newUser.birthday = profile.birthday;    // app must be reviewed by facebook in order to get permission for birthday
+            }
+
+            // save user to database
+            newUser.save(function (err) {
+                if (err) {
+                    throw err;
+                } else {
+                    return done(null, newUser);
+                }
+            });
+        })
     })
 )
