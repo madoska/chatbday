9bf9df373e69c84649539a757dd712364ca75c25
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-29 15:11:11 +0200

    installed mongoose + added require authController to routes/user.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const usersRouter = express.Router();
+var express = require('express');
+var router = express.Router();
+const authController = require('../controllers/auth');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = usersRouter;
+module.exports = router;
